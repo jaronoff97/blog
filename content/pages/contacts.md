afda3e54662b146817b910cfde8d21244447b8de
Jacob Aronoff <jacobaronoff45@gmail.com>
2022-09-11 22:53:52 -0400

    Git ignoring

--- content/pages/contacts.md
+++
@@ -1,6 +0,0 @@
----
-title: "Contact me"
-template: "page"
----
-
-I'd love to chat! Reach out on any social media platform, or [email me here](mailto:me@jaronoff.com)
--- gatsby-node.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-
-const express = require('express')
-exports.onCreateDevServer=({app})=>{
-    app.use(express.static('public'))
-}
-exports.createPages = require('./gatsby/create-pages');
-exports.onCreateNode = require('./gatsby/on-create-node');
